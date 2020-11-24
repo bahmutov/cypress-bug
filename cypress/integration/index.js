f7467c3e28d599f1743e4890c5cfcbb745b2b550
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-24 09:45:41 -0500

    run test 100 times

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,13 +1,16 @@
+/// <reference types="cypress" />
 describe("cypress bug", () => {
-	it("should fail sometimes, rerun if not ;)", () => {
-		cy.server();
-		cy.route("get", "/api/posts").as("getPosts");
+	Cypress._.times(100, (k) => {
+		it(`test ${k} of 100`, () => {
+			cy.server();
+			cy.route("get", "/api/posts").as("getPosts");
 
-		cy.visit("http://localhost:3000");
+			cy.visit("http://localhost:3000");
 
-		cy.wait("@getPosts");
-		cy.wait("@getPosts");
-		cy.wait("@getPosts");
-		cy.wait("@getPosts");
-	});
+			cy.wait("@getPosts");
+			cy.wait("@getPosts");
+			cy.wait("@getPosts");
+			cy.wait("@getPosts");
+		});
+	})
 });
